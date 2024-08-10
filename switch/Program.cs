25a14b3234643dce5fe6bfa0422ea42be89a2e5e
farid <farid@LAPTOP-SPTVSQVM>
2024-08-10 10:16:48 +0530

    Add project files.

---
+++ switch/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace using_switch
+{
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+            int n, a, b;
+            var c = new Switch_statement();
+            Console.WriteLine("enter the value of a:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the value of b:");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("operations available :" +
+                " 1. Addition " +
+                "2. Subtraction " +
+                "3. Multiplication " +
+                "4. Division");
+            n = int.Parse(Console.ReadLine());
+            c.Calc(n, a, b);    
+        }
+    }
+}
---
+++ switch/Switch_statement.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace using_switch
+{
+    public class Switch_statement
+    {
+        public void Calc(int n, int a, int b)
+        {
+            if (n == 0)
+            {
+                Console.WriteLine("0 is not a valid option");
+            }
+            else
+            {
+                switch (n)
+                {
+                    case 1:
+                        Console.WriteLine($"{a} + {b} = {a + b} ");
+                        break;
+                    case 2:
+                        Console.WriteLine($"{a} - {b} = {a - b}" );
+                        break;
+                    case 3:
+                        Console.WriteLine($"{a} * {b} = {a * b}");
+                        break;
+                    case 4:
+                        Console.WriteLine($"{a} * {b} = {a / b}");
+                        break;
+                    default:
+                        Console.WriteLine("choose a valid option");
+                        break;
+                }
+            }
+        }
+    }
+}
